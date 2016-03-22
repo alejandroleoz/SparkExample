32926105f6b0b9b1e47cedd3c4cc4e064515f5a8
Alejandro Leoz <alejandro.leoz@sovanta.com>
2016-03-22 15:25:34 -0300

    Initial commit for Spark + Velocity demo

---
+++ src/main/java/mago/examples/App.java
@@ -0,0 +1,49 @@
+package mago.examples;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import static spark.Spark.*;
+
+
+
+/**
+ * Spark Example 
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+
+        get("/hello", (req, res) -> "Hello World");
+
+
+        get("/velocity", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("hello", "Velocity World");
+            model.put("person", new Person("Foobar"));
+
+            // The wm files are located under the resources directory
+            return new ModelAndView(model, "hello.vm");
+        }, new VelocityTemplateEngine());
+    }
+
+    public static class Person {
+        private String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
+}
